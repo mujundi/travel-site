f8677f3ae765126e5c1f838ae1e8475e394f319f
mujundi <mjundi@ufl.edu>
2018-09-27 05:12:19 -0400

    Finished implementing automatic sprite generation via Gulp.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
+del = require('del');
 
 var config = {
+	shape: {
+		spacing: {
+			padding: 1
+		}
+	},
 	mode: {
 		css: {
+			sprite: 'sprite.svg',
 			render: {
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -13,8 +21,29 @@ var config = {
 	}
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.css')
+		.pipe(rename('_sprite.css'))
+		.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteCSS', 'copySpriteGraphic'], function() {
+	return del(['./app/temp/sprite']);
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
